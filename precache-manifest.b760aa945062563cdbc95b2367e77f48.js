031fbb398a01f86e9c39bf35e52363823f800fa5
xakim <xakim@pop-os.localdomain>
2020-10-14 12:00:36 +0600

    Updates

---
+++ precache-manifest.b760aa945062563cdbc95b2367e77f48.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9058494fe2337a4a8b863746a8fb540",
+    "url": "/parliament-calculator-v2/index.html"
+  },
+  {
+    "revision": "9860ff4d971624d09465",
+    "url": "/parliament-calculator-v2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c0be65033bab22872c9",
+    "url": "/parliament-calculator-v2/static/js/2.fa89424c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/parliament-calculator-v2/static/js/2.fa89424c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9860ff4d971624d09465",
+    "url": "/parliament-calculator-v2/static/js/main.0e377377.chunk.js"
+  },
+  {
+    "revision": "69e0b071b85c2da532eb",
+    "url": "/parliament-calculator-v2/static/js/runtime-main.2e01dc1e.js"
+  },
+  {
+    "revision": "42ccabd34e43cc9f80495d75581c8096",
+    "url": "/parliament-calculator-v2/static/media/PARTIES_RESULTS_REACT_LEVEL_ONE.42ccabd3.csv"
+  },
+  {
+    "revision": "d6ae188912c1b4c4c29c4de7342d12f1",
+    "url": "/parliament-calculator-v2/static/media/Бир Бол.d6ae1889.png"
+  },
+  {
+    "revision": "5f98b378fce0a6bd8d166083d5432ee3",
+    "url": "/parliament-calculator-v2/static/media/Кыргызстан.5f98b378.png"
+  },
+  {
+    "revision": "b9d0c5d3dc30b0d0bce829e7b98c7807",
+    "url": "/parliament-calculator-v2/static/media/Мекен Ынтымагы.b9d0c5d3.png"
+  },
+  {
+    "revision": "d975dd8f04f9be16f265c3983a306475",
+    "url": "/parliament-calculator-v2/static/media/Политическая партия ветеранов войны в Афганистане и участников других боевых конфликтов.d975dd8f.png"
+  },
+  {
+    "revision": "3f7000b1ad13ad7b4a33f2cefb24a9f0",
+    "url": "/parliament-calculator-v2/static/media/Чон казат.3f7000b1.png"
+  },
+  {
+    "revision": "e1489fd6f4537782821aecc61003e5a5",
+    "url": "/parliament-calculator-v2/static/media/Ыйман Нуру.e1489fd6.png"
+  }
+]);
